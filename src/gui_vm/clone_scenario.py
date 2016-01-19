96471cf8d3f23ba30bdfbb3f9e0fbea2fb0e0d66
Christoph Franke <christoph.franke@web.de>
2016-01-19 12:16:25 +0100

    Fixed: entry point for cloning, moved code from main to clone_scenario script

--- src/gui_vm/clone_scenario.py
+++ src/gui_vm/clone_scenario.py
@@ -11,27 +11,23 @@ def clone():
                         help="vorhandene XML-Projektdatei öffnen",
                         dest="project_file")
 
-    parser.add_argument("-t", '--template', action="store",
-                        help="angegebenes Szenario klonen",
-                        dest="template_name")
+    parser.add_argument("-t", action="store",
+                        help="Name des zu klonenden Szenarios",
+                        dest="template")
 
-    parser.add_argument("-s", '--scenario', action="store",
-                        help="neuer Szenario-Name",
-                        dest="scenario_name")
+    parser.add_argument("-s", action="store",
+                        help="Name des zu erstellenden, geklonten Szenarios",
+                        dest="new_scenario")
 
     arguments = parser.parse_args()
 
     app = QtGui.QApplication(sys.argv)
     project_file = arguments.project_file
     template = arguments.template
-    new_scenario = arguments.scenario_name
-
-    # hier die Clone-Methode aufrufen!
-
-    #mainwindow = MainWindow(project_file = project_file,
-                            #run_scenario = run_scenario)
-    #mainwindow.show()
-    #sys.exit(app.exec_())
+    new_scenario_name = arguments.new_scenario
+    mainwindow = MainWindow(project_file = project_file)
+    mainwindow.show()
+    mainwindow.clone_scenario(template, new_scenario_name)
 
 if __name__ == "__main__":
     clone()
