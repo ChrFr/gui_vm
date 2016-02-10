f6e32ef3f19d93b4658c34ea70da38f070e9fbd2
MaxBo <max.bohnet@tuhh.de>
2016-02-10 14:21:07 +0100

    validate_scenarios returns the selected run

--- src/gui_vm/validate_scenario.py
+++ src/gui_vm/validate_scenario.py
@@ -2,7 +2,7 @@
 from argparse import ArgumentParser
 import sys
 from gui_vm.control.main_control import MainWindow
-from gui_vm.model.project_tree import Scenario
+from gui_vm.model.project_tree import Scenario, OutputNode
 from PyQt4 import QtGui
 
 def startmain():
@@ -21,27 +21,30 @@ def startmain():
     app = QtGui.QApplication(sys.argv)
     project_file = arguments.project_file
     scenario_name = arguments.scenario_name
+    ret = ''
 
     mainwindow = MainWindow.only_validate(project_file, scenario_name)
-    while not mainwindow.validated:
-        mainwindow.show()
-        ret = app.exec_()
-        selected_item = mainwindow.project_control.selected_item
-        try:
-            while not isinstance(selected_item, Scenario):
-                selected_item = selected_item.parent
-            else:
-                scenario_name = selected_item.name
-                if selected_item.is_valid:
-                    ret ='selected scenario:{sc}'.format(sc=scenario_name)
-                else:
-                    ret = 'scenario {sc} invalid'.format(sc=scenario_name)
-                mainwindow.validated = True
-        except AttributeError:
-            ret = 'no scenario selected'
-            # ggf. Abbruch-Button hinzufügen...
-    else:
-        ret = 'selected scenario:{sc}'.format(sc=scenario_name)
+    mainwindow.show()
+    ret = app.exec_()
+    if ret < 0:
+        raise RuntimeError('The GUI caused a problem')
+    ret = ''
+    selected_item = mainwindow.project_control.selected_item
+    try:
+        while not isinstance(selected_item, (Scenario, OutputNode)):
+            selected_item = selected_item.parent
+        else:
+            if isinstance(selected_item, OutputNode):
+                selected_run = selected_item.name
+                ret = 'selected run:{run}\n'.format(run=selected_run)
+                while not isinstance(selected_item, Scenario):
+                    selected_item = selected_item.parent
+            scenario_name = selected_item.name
+            ret += 'selected scenario:{sc}'.format(sc=scenario_name)
+            mainwindow.validated = True
+    except AttributeError:
+        ret = 'no scenario selected'
+        # ggf. Abbruch-Button hinzufügen...
     sys.exit(ret)
 
 
